3f6e48b812b8b3ba9a0b256963e2d3f83825187d
Sergey Voytov <sergvoytov@gmail.com>
2020-01-15 21:30:51 -0800

    lab2

--- basiclibrary/src/test/java/basiclibrary/LibraryTest.java
+++ basiclibrary/src/test/java/basiclibrary/LibraryTest.java
@@ -3,31 +3,62 @@
  */
 package basiclibrary;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class LibraryTest {
-    @Test
-    public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-    }
-
 
     @Test
-    public void testRollMethod() {
-int number = 4;
-int[] result = Library.roll(number);
-assertEquals(number,result.length);
+    public void testRoll() {
+        int number = 3;
+        int[] result = Library.roll(number);
+        assertEquals(number, result.length);
     }
 
-    @Test public void testRollBetweenOneAndSix(){
+    @Test
+    public void testRollBetweenOneToSix() {
         int[] result = Library.roll(3);
-      for(int number : result ){
-          boolean actual = number > 0 && number<7;
-          assertTrue(actual);
-      }
+        for (int number : result) {
+            boolean actual = number > 0 && number < 7;
+            assertTrue(actual);
+        }
+    }
 
+    @Test
+    public void testContainsDuplicatesFalse() {
+        int[] array = {12, 24, 55, 66, 90};
+        boolean isDuplicate = Library.containsDuplicates(array);
+        assertFalse(isDuplicate);
+    }
+
+    @Test
+    public void testContainsDuplicatesTrue() {
+        int[] array = {16, 16, 25, 43, 90, 1009};
+        boolean isTrue = Library.containsDuplicates(array);
+        assertTrue(isTrue);
+    }
+
+    @Test
+    public void testCalculatingAverages() {
+        int[] array = {15, 43, 52, 65, 86, 37, 56};
+        double average = Library.calculatingAverages(array);
+        double expected = 50.57143;
+        assertEquals(expected, average, 0.001);
+    }
+
+    @Test
+    public void testArraysOfArrays() {
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        int sum = Library.arraysOfArrays(weeklyMonthTemperatures);
+        int expected = 57;
+        assertEquals(expected, sum);
     }
 }
+
